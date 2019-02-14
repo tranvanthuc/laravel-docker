de8fd43b685efa3cf471fc650ad5976e2bcb6554
Tran Van Thuc <tranvanthuc365@gmail.com>
2019-02-14 21:08:05 +0700

    Update infinite scroll

--- resources/js/article.js
+++ resources/js/article.js
@@ -1,88 +1,132 @@
-import axios from 'axios';
+import axios from "axios";
 
 class Article {
-  constructor() {
-    this.config();
-    this.listen();
-    this.size = 10;
-  }
-
-  config() {
-    this.apiUrl = 'http://localhost:8080/api/';
-    this.element = {
-      input: {
-        search: $('#search')
-      },
-      button: {
-        loadMore: $('#load-more')
-      },
-      body: $('.panel-body')
-    };
-  }
-
-  listen() {
-    this.search(this.size);
-    this.loadMore();
-    this.handleOnScroll();
-  }
-
-  handleOnScroll() {
-    const self = this;
-    window.onscroll = function () {
-      const { element: { input: { search: inputSearch }} } = self;
-      const value = inputSearch.val();
-      let scrollTop = (document.documentElement && document.documentElement.scrollTop) || document.body.scrollTop;
-      let scrollHeight = (document.documentElement && document.documentElement.scrollHeight) || document.body.scrollHeight;
-      let clientHeight = document.documentElement.clientHeight || window.innerHeight;
-      let scrolledToBottom = Math.ceil(scrollTop + clientHeight) >= scrollHeight;
-
-      // scroll load more
-      if (scrolledToBottom) {
-        self.size += 10;
-        self.fetchArticles(value, self.size)
-      }
-    }
-  }
-
-  loadMore() {
-    const { element: { input: { search: inputSearch }, button: { loadMore: btnLoadMore } } } = this;
-    const value = inputSearch.val();
-    // click load more
-    btnLoadMore.click(() => {
-      this.size += 10;
-      this.fetchArticles(value, this.size)
-    });
-
-  }
-
-  fetchArticles(value, size) {
-    const { apiUrl, element: { body } } = this;
-    axios({
-      method: 'get',
-      url: apiUrl + 'article',
-      params: {
-        query: value,
-        size: size
-      }
-    }).then(({ data }) => {
-      body.html(data)
-    })
-      .catch(error => {
-        console.log(error);
-      })
-  }
-
-  search(size) {
-    const { element: { input } } = this;
-    let inputSearch = input.search;
-    inputSearch.keydown((e) => {
-      if (e.keyCode === 13) {
-        const { target: { value } } = e;
+    constructor() {
+        this.config();
+        this.listen();
         this.size = 10;
-        this.fetchArticles(value, size)
-      }
-    })
-  }
+        this.count = 0;
+    }
+
+    config() {
+        this.apiUrl = "http://localhost:8080/api/";
+        this.element = {
+            input: {
+                search: $("#search")
+            },
+            button: {
+                loadMore: $("#load-more")
+            },
+            body: $(".panel-body"),
+            list: $("#list-article")
+        };
+    }
+
+    listen() {
+        this.search(this.size);
+        this.loadMore();
+        this.handleOnScroll();
+        // this.newScroll();
+    }
+
+    upCount() {
+        this.count++;
+    }
+
+    resetCount() {
+        this.count = 0;
+    }
+
+    handleOnScroll() {
+        const self = this;
+        window.onscroll = function() {
+            let scrollTop =
+                (document.documentElement &&
+                    document.documentElement.scrollTop) ||
+                document.body.scrollTop;
+            let scrollHeight =
+                (document.documentElement &&
+                    document.documentElement.scrollHeight) ||
+                document.body.scrollHeight;
+            let clientHeight =
+                document.documentElement.clientHeight || window.innerHeight;
+            const offsetBottom = 400;
+            let scrolledToBottom =
+                Math.ceil(scrollTop + clientHeight) >=
+                scrollHeight - offsetBottom;
+
+            const {
+                element: {
+                    input: { search: inputSearch }
+                }
+            } = self;
+            const value = inputSearch.val();
+            // scroll load more
+            if (scrolledToBottom) {
+                self.upCount();
+                if (self.count === 1) {
+                    console.log("call aPI");
+                    self.size += 10;
+                    self.fetchArticles(value, self.size);
+                }
+                console.log("count", self.count);
+            }
+        };
+    }
+
+    loadMore() {
+        const {
+            element: {
+                input: { search: inputSearch },
+                button: { loadMore: btnLoadMore }
+            }
+        } = this;
+        const value = inputSearch.val();
+        // click load more
+        btnLoadMore.click(() => {
+            this.size += 10;
+            this.fetchArticles(value, this.size);
+        });
+    }
+
+    fetchArticles(value, size) {
+        const self = this;
+        const {
+            apiUrl,
+            element: { body }
+        } = this;
+        axios({
+            method: "get",
+            url: apiUrl + "article",
+            params: {
+                query: value,
+                size: size
+            }
+        })
+            .then(({ data }) => {
+                body.html(data);
+                self.resetCount();
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    }
+
+    search(size) {
+        const {
+            element: { input }
+        } = this;
+        let inputSearch = input.search;
+        inputSearch.keydown(e => {
+            if (e.keyCode === 13) {
+                const {
+                    target: { value }
+                } = e;
+                this.size = 10;
+                this.fetchArticles(value, size);
+            }
+        });
+    }
 }
 
 new Article();
--- resources/js/firebase.js
+++ resources/js/firebase.js
@@ -1,63 +1,49 @@
-import firebase from 'firebase';
-import functions from 'firebase/functions';
+import firebase from "firebase";
 
 class Firebase {
-  constructor() {
-    this.init();
-  }
+    constructor() {
+        this.init();
+    }
 
-  init() {
-    this.config();
-    this.listen();
-  }
+    init() {
+        this.config();
+        this.listen();
+    }
 
-  config() {
-    const config = $('#config');
-    const configFirebase = {
-      apiKey: config.data('firebaseApiKey'),
-      authDomain: config.data('firebaseProjectId') + ".firebaseapp.com",
-      databaseURL: config.data('firebaseDatabaseUrl'),
-      projectId: config.data('firebaseProjectId'),
-      storageBucket: config.data('firebaseBucket'),
-      messagingSenderId: config.data('firebaseSendId'),
-    };
-    firebase.initializeApp(configFirebase);
-    this.database = firebase.database();
+    config() {
+        const config = $("#config");
+        const configFirebase = {
+            apiKey: config.data("firebaseApiKey"),
+            authDomain: config.data("firebaseProjectId") + ".firebaseapp.com",
+            databaseURL: config.data("firebaseDatabaseUrl"),
+            projectId: config.data("firebaseProjectId"),
+            storageBucket: config.data("firebaseBucket"),
+            messagingSenderId: config.data("firebaseSendId")
+        };
+        firebase.initializeApp(configFirebase);
+        this.database = firebase.database();
+        this.firestore = firebase.firestore();
+    }
 
-  }
+    listen() {
+        this.trigger();
+    }
 
-  listen() {
-    this.trigger();
-  }
-
-  trigger() {
-    console.log("Hello");
-
-    // this.database.ref('notifications').orderByChild('user_id').equalTo(1).on('value', snapshot => {
-    //   console.log(snapshot.val())
-    // }, errorObject => {
-    //   console.log(errorObject);
-    // })
-    functions.database.ref('notifications/{noticeId}')
-      .onWrite((change, context) => {
-        console.log(context.params.noticeId);
-        if (change.before.exists()) {
-          return null;
-        }
-        // Exit when the data is deleted.
-        if (!change.after.exists()) {
-          return null;
-        }
-        // Grab the current value of what was written to the Realtime Database.
-        const original = change.after.val();
-        console.log('Uppercasing', context.params.pushId, original);
-        const uppercase = original.toUpperCase();
-        // You must return a Promise when performing asynchronous tasks inside a Functions such as
-        // writing to the Firebase Realtime Database.
-        // Setting an "uppercase" sibling in the Realtime Database returns a Promise.
-        return change.after.ref.parent.child('uppercase').set(uppercase);
-      })
-  }
+    trigger() {
+        this.firestore
+            .collection("users")
+            .add({
+                first: "thuc",
+                last: "tran",
+                gender: "male"
+            })
+            .then(function(docRef) {
+                console.log("Document written with ID: ", docRef.id);
+            })
+            .catch(function(error) {
+                console.error("Error adding document: ", error);
+            });
+    }
 }
 
 new Firebase();
